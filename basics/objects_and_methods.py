f9b5563f8202ee40c56c87453bc75fd12a2e5203
Shaan Alam <shaanalam369@gmail.com>
2020-11-27 16:46:07 +0530

    string formatting

---
+++ basics/objects_and_methods.py
@@ -0,0 +1,8 @@
+n = 4
+
+# In python every string, integer and all other variables are objects
+# To find the id of the object, use id()
+# To find the type of object, use type()
+
+print (id(n))
+print (type(n))
---
+++ basics/strings.py
@@ -0,0 +1,21 @@
+string = 'Welcome'
+
+# lower() => Converts all letters to lowercase
+print (string.lower())
+
+# upper() => Converts all letters to uppercase
+print (string.upper())
+
+# strip() => remove whitespace characters from both the ends of the string
+print (string.strip()) 
+
+# Formatting numbers and strings
+
+# Formatting with scientific notation
+print (format(57.45679, "9.3e"))
+
+# Formatting with float values
+print (format(57.456, '10.3f'))
+
+# Formatting with % => multiply number with 100 and formats the string
+print (format(57.456, '10.2%'))
