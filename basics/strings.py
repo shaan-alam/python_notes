19f674dc951dd24434cd6bd3d00ca0f7adde8cd0
Shaan Alam <shaanalam369@gmail.com>
2020-11-28 14:14:13 +0530

    completed string formatting

--- basics/strings.py
+++ basics/strings.py
@@ -19,3 +19,23 @@ print (format(57.456, '10.3f'))
 
 # Formatting with % => multiply number with 100 and formats the string
 print (format(57.456, '10.2%'))
+
+# Justifying formats => You can justify the strings with > for right justification and < for left justification
+print (format(54.74564, "<10.2f"))
+
+# Formatting integers
+# d = decimal
+# o = octal
+# x = hexadecimal
+# b = binary
+
+print (format(78656, "10d"))
+print (format(78656, "<10d"))
+print (format(78656, "10x"))
+print (format(78656, "<10x"))
+print (format(78656, "10x"))
+
+# Formatting strings
+print (format("Shaan ", ">20s"))
+
+print (format(0.457467657, "9.3%"))
