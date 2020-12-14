4ac7d412f64809703206c0155c609df49e9c658b
Shaan Alam <shaanalam369@gmail.com>
2020-12-14 21:34:37 +0530

    string functions

--- python-course/chapter-3/01_strings.py
+++ python-course/chapter-3/01_strings.py
@@ -9,3 +9,15 @@ print (name[0:3]) # => print name from 0 to 3
 print (name[0:]) # => print name from 0 to end
 print (name[:]) # => print whole string
 print (name[:4]) # => print name from 0 to 3 (4 - 1)
+
+# negative indexes
+# We can access string character's from behind using negative indexing
+
+print ("Last character : ", name[-1])
+
+# Slicing with negative indexing
+print (name[-5:-4])
+
+# Slicing with skip value
+# name[start:end:skip_value]
+print (name[2:5:2]) # printing name from 2 to 4 by skipping 2 values
---
+++ python-course/chapter-3/02_string_functions.py
@@ -0,0 +1,22 @@
+# string functions
+
+story = "Once upon a time, there was a code named Shaan Alam who learned Python"
+
+# string functions
+print (len(story))
+
+# endswith()
+print (story.endswith("Python")) # True
+
+# count() => count the number of characters
+print (story.count(' '))
+print (story.count("Shaan"))
+
+# capitalize() => capitalizes the string
+print (story.capitalize())
+
+# find() => finds a word in the string
+print (story.find('Shaan')) # gives only the first occurence
+
+# replace() => replace a string
+print (story.replace('Shaan', 'shaancodes')) # replaces every occurence
