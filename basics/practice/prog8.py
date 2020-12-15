146438b860ff69d38114c36ab6d0f54f24055809
Shaan Alam <shaanalam369@gmail.com>
2020-12-15 12:14:54 +0530

    completed program

--- basics/practice/prog8.py
+++ basics/practice/prog8.py
@@ -1,7 +1,13 @@
 
 def isLeapYear(year):
-    if (year % 4 == 0 and year % 100 != 0) or year % 400 == 0:
-        return True
+    if year % 4 == 0:
+        if year % 100 == 0:
+            if year % 400 == 0:
+                return True
+            else: 
+                return False
+        else:
+            return True 
     else:
         return False
 
