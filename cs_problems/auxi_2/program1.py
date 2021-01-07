65052d5d5de5feb486b0988aca758edd8649cd4a
Shaan Alam <shaanalam369@gmail.com>
2021-01-07 21:54:55 +0530

    commit

---
+++ cs_problems/auxi_2/program1.py
@@ -0,0 +1,28 @@
+# Question 1
+# Find the common characters in the string
+
+# Commit on jan 7
+
+string1 = input("Enter the string 1 : ")
+string2 = input("Enter the string 2 : ")
+
+print ("String 1 :", string1)
+print ("String 2 :", string2)
+
+count = 0
+
+print ("Common characters are : ")
+
+dup = "" # to store the duplicate characters
+
+for x in string1:
+  if x in string2 and dup.find(x) == -1:
+    print (x)
+    dup += x
+    count += 1
+
+
+if count == 0:
+  print ("There are no common characters in both strings.. ")
+else:
+  print ("There are ",count,"common characters in the string.")
---
+++ cs_problems/auxi_2/program2.py
@@ -0,0 +1,12 @@
+# Question 2
+# Find the reverse of the string
+
+string = input("Enter the string: ")
+
+reverse = ""
+
+for x in range(len(string) - 1, -1, -1):
+  reverse += string[x]
+
+print ("Original string: ", string)
+print ("Reversed string: ", reverse)
